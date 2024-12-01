7f1988f3601ab30e7f7d3f9d87e5274472442c4f
Samarth Shinde <samarthsshinde@gmail.com>
2024-12-01 11:26:44 +0530

    student dashboard design

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,3 +1,21 @@
+import Announcement from "@/components/Announcement";
+import EventCalendar from "@/components/EventCalendar";
+
 export default function StudentPage() {
-	return <>student page</>;
+	return (
+		<div className="p-4 flex gap-4 flex-col xl:flex-row">
+			{/* LEFT */}
+			<div className="w-full xl:w-2/3">
+				<div className="h-full bg-white rounded-md p-4">
+					<h1 className="text-xl font-semibold">Schedule (4A)</h1>
+				</div>
+			</div>
+
+			{/* RIGHT */}
+			<div className="w-full xl:w-1/3 flex flex-col gap-8">
+				<EventCalendar />
+				<Announcement />
+			</div>
+		</div>
+	);
 }
