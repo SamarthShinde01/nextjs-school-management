eb1b88bd83b3cfc1967b66a6ea3ce1f89c33e8cd
Samarth Shinde <samarthsshinde@gmail.com>
2024-12-10 17:13:52 +0530

    admin homepage data

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,6 +1,20 @@
+import prisma from "@/lib/prisma";
 import Image from "next/image";
 
-export default function UserCard({ type }: { type: string }) {
+type UserCardTypes = {
+	type: "admin" | "teacher" | "student" | "parent";
+};
+
+export default async function UserCard({ type }: UserCardTypes) {
+	const modelMap: Record<typeof type, any> = {
+		admin: prisma.admin,
+		teacher: prisma.teacher,
+		student: prisma.student,
+		parent: prisma.parent,
+	};
+
+	const data = await modelMap[type].count();
+
 	return (
 		<div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1 min-w-[130px]">
 			<div className="flex items-center justify-between">
@@ -9,7 +23,7 @@ export default function UserCard({ type }: { type: string }) {
 				</span>
 				<Image src="/more.png" alt="" width={20} height={20} />
 			</div>
-			<h1 className="text-2xl font-semibold my-4">1,234</h1>
+			<h1 className="text-2xl font-semibold my-4">{data}</h1>
 			<h2 className="capitalize text-sm font-medium text-gray-500">{type}</h2>
 		</div>
 	);
