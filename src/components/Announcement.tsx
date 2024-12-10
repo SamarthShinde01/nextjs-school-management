177a0a14c2d10c7a352bc549e76205b1520cb080
Samarth Shinde <samarthsshinde@gmail.com>
2024-12-10 20:33:00 +0530

    fetch homepage announcements

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -1,4 +1,26 @@
-export default function Announcement() {
+import prisma from "@/lib/prisma";
+import { currentUserId, role } from "@/lib/utils";
+
+export default async function Announcement() {
+	const roleConditions = {
+		teacher: { lessons: { some: { teacherId: currentUserId! } } },
+		student: { students: { some: { id: currentUserId! } } },
+		parent: { students: { some: { parentId: currentUserId! } } },
+	};
+
+	const data = await prisma.announcement.findMany({
+		take: 3,
+		orderBy: { date: "desc" },
+		where: {
+			...(role !== "admin" && {
+				OR: [
+					{ classId: null },
+					{ class: roleConditions[role as keyof typeof roleConditions] || {} },
+				],
+			}),
+		},
+	});
+
 	return (
 		<div className="p-4 bg-white rounded-md">
 			<div className="flex items-center justify-between">
@@ -7,44 +29,44 @@ export default function Announcement() {
 			</div>
 
 			<div className="flex flex-col gap-4 mt-4">
-				<div className="bg-lamaSkyLight rounded-md p-4 ">
-					<div className="flex items-center justify-between">
-						<h2 className="font-medium">Lorem ipsum dolor sit</h2>
-						<span className="text-xs text-gray-400 bg-white px-1 py-1 rounded-md ">
-							2024-01-01
-						</span>
+				{data[0] && (
+					<div className="bg-lamaSkyLight rounded-md p-4 ">
+						<div className="flex items-center justify-between">
+							<h2 className="font-medium">{data[0]?.title}</h2>
+							<span className="text-xs text-gray-400 bg-white px-1 py-1 rounded-md ">
+								{data[0]?.date.toLocaleDateString()}
+							</span>
+						</div>
+
+						<p className="text-xs text-gray-400 mt-1">{data[0]?.description}</p>
 					</div>
+				)}
 
-					<p className="text-xs text-gray-400 mt-1">
-						Lorem ipsum dolor sit amet, concesteum adipuscing elit,
-					</p>
-				</div>
+				{data[1] && (
+					<div className="bg-lamaPurpleLight rounded-md p-4 ">
+						<div className="flex items-center justify-between">
+							<h2 className="font-medium">{data[1]?.title}</h2>
+							<span className="text-xs text-gray-400 bg-white px-1 py-1 rounded-md ">
+								{data[1]?.date.toLocaleDateString()}
+							</span>
+						</div>
 
-				<div className="bg-lamaPurpleLight rounded-md p-4 ">
-					<div className="flex items-center justify-between">
-						<h2 className="font-medium">Lorem ipsum dolor sit</h2>
-						<span className="text-xs text-gray-400 bg-white px-1 py-1 rounded-md ">
-							2024-01-01
-						</span>
+						<p className="text-xs text-gray-400 mt-1">{data[1]?.description}</p>
 					</div>
+				)}
 
-					<p className="text-xs text-gray-400 mt-1">
-						Lorem ipsum dolor sit amet, concesteum adipuscing elit,
-					</p>
-				</div>
+				{data[2] && (
+					<div className="bg-lamaYellowLight rounded-md p-4 ">
+						<div className="flex items-center justify-between">
+							<h2 className="font-medium">{data[2]?.title}</h2>
+							<span className="text-xs text-gray-400 bg-white px-1 py-1 rounded-md ">
+								{data[2]?.date.toLocaleDateString()}
+							</span>
+						</div>
 
-				<div className="bg-lamaYellowLight rounded-md p-4 ">
-					<div className="flex items-center justify-between">
-						<h2 className="font-medium">Lorem ipsum dolor sit</h2>
-						<span className="text-xs text-gray-400 bg-white px-1 py-1 rounded-md ">
-							2024-01-01
-						</span>
+						<p className="text-xs text-gray-400 mt-1">{data[2]?.description}</p>
 					</div>
-
-					<p className="text-xs text-gray-400 mt-1">
-						Lorem ipsum dolor sit amet, concesteum adipuscing elit,
-					</p>
-				</div>
+				)}
 			</div>
 		</div>
 	);
